1e46f1c888799c2019d5ac998408fe3e5c58e026
Ardin Biganiro <ardinbig@gmail.com>
2026-04-27 17:59:55 +0200

    chore: add 'storm_api_client' initial project files

---
+++ packages/storm_api_client/lib/src/storm_api_client.dart
@@ -0,0 +1,7 @@
+/// {@template storm_api_client}
+/// Dio-based HTTP client for the Storm REST API
+/// {@endtemplate}
+class StormApiClient {
+  /// {@macro storm_api_client}
+  const StormApiClient();
+}
