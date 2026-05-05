5b5657b9cda71b6f325dc28538f3351f64022ca0
Ardin Biganiro <ardinbig@gmail.com>
2026-05-05 23:43:57 +0200

    feat(storm_api_client): add category and metrics response models

--- packages/storm_api_client/lib/src/models/common/common.dart
+++ packages/storm_api_client/lib/src/models/common/common.dart
@@ -1,1 +1,4 @@
+export 'category.dart';
+export 'create_category_request.dart';
 export 'error_response.dart';
+export 'metrics_response.dart';
---
+++ packages/storm_api_client/lib/src/models/common/metrics_response.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'metrics_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MetricsResponse _$MetricsResponseFromJson(Map<String, dynamic> json) =>
+    MetricsResponse(requests: (json['requests'] as num).toInt());
+
+Map<String, dynamic> _$MetricsResponseToJson(MetricsResponse instance) =>
+    <String, dynamic>{'requests': instance.requests};
