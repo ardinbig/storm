06f937956fb0cc60d0d0b821a24b351ebe59de53
Ardin Biganiro <ardinbig@gmail.com>
2026-05-18 14:31:46 +0200

    chore: add 'token_storage' initial project files

---
+++ packages/token_storage/test/src/token_storage_test.dart
@@ -0,0 +1,13 @@
+// Not required for test files
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:token_storage/token_storage.dart';
+
+void main() {
+  group('TokenStorage', () {
+    test('can be instantiated', () {
+      expect(TokenStorage(), isNotNull);
+    });
+  });
+}
