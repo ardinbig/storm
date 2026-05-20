76133d68e2f355d0f53a38782088f678709cea3b
Ardin Biganiro <ardinbig@gmail.com>
2026-05-20 21:52:11 +0200

    chore: add 'authentication_repository' initial project files

---
+++ packages/authentication_repository/lib/authentication_repository.dart
@@ -0,0 +1,4 @@
+/// Repository that manages authentication state for the Storm app.
+library;
+
+export 'src/authentication_repository.dart';
---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// Repository that manages authentication state for the Storm app.
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}
